6103372c010dd081b5ffc443a5dff5368ded0e39
Zosia <sobocinska.zosia@gmail.com>
2013-11-05 18:22:49 +0100

    Rewritten server.pad.endec functions.

--- server/pad/endec.py
+++ server/pad/endec.py
@@ -4,17 +4,27 @@
 """
 from pad import codes
 
-DELIMITER = u"\u001F"
+UNIT_DELIMTR = 2 * u"\u001F"
+INNER_DELIMTR = u"\u001F"
+
 
 def decode(record):
-#    return dict((unit[0], (len(unit) == 1 or unit[1].decode('base64'))) for unit in [unit.split(DELIMITER) for unit in record.split(2 * DELIMITER)])
     data = {}
-    units = {unit for unit in record.split(2 * DELIMITER)}
+    units = {unit for unit in record.split(UNIT_DELIMTR)}
     for unit in units:
-        pair = unit.split(DELIMITER)
-        data[codes.label(int(pair[0]))] = len(pair) == 1 or pair[1].decode('base64')
+        pair = unit.split(INNER_DELIMTR)
+        key = None
+        try:
+            key = int(pair[0])
+            key = codes.label(key)
+        except Exception:  # handeled in the following if
+            pass
+        if not key:
+            key = str(pair())
+
+        data[key] = len(pair) == 1 or pair[1].decode('base64')
     return data
 
 
 def encode(data):
-    return (2 * DELIMITER).join("%s%s" % (str(codes.code(key)), "" if data[key] == True else DELIMITER + data[key].encode('base64')) for key in data)
+    return (UNIT_DELIMTR).join("%s%s%s" % (str(codes.code(key)), "" if data[key] == True else INNER_DELIMTR, data[key].encode('base64') if data[key] != True  else "") for key in data)
