afb3fbdaacc2c4ed580e29629029c3e4716d56dd
Zosia <sobocinska.zosia@gmail.com>
2013-11-06 02:47:33 +0100

    Message passing endec extended by protocol version number.

--- server/pad/endec.py
+++ server/pad/endec.py
@@ -10,28 +10,42 @@ message encoded by pad.endec.encode() may require further encapsulation
 processing before sending and, analogically, before decoding received message
 it may be required to decapsulate it first.
 
-Data record string sent/received via pad socket mechanism consists of logical
-<i>units</i> joined by UNIT_DELIMTR characters sequence. <i>Unit</i> may be:
+Data record string sent/received via pad socket mechanism consists of single-
+character version number (version number encoded by chr(key_int) and decoded
+by ord(key_char)) followed by sequence of logical <i>units</i> joined by
+UNIT_DELIMTR characters sequence. <i>Unit</i> may be:
 - either <b>key-value</b> pair,
 - or <b>flag key</b>.
 
-\par Key-value pair
+\par Key-value pair unit
 If INNER_DELIMTR char sequence is present in unit content, then we handle
-<b>key-value</b> pair. Key and value are separated by UNIT_DELIMTR. Key part
-should be handled as-is, whereas value part is transmitted base64-encoded.
+<b>key-value</b> pair. Key and value are separated by UNIT_DELIMTR. Key is
+represented as described in "Keys" paragraph. Key character/string is
+transmitted as-is, whereas value string is transmitted base64-encoded.
 
-\par Flag key
+\par Flag key unit
 If INNER_DELIMTR char sequence isn't present in unit content, then we handle
 <b>flag key</b>. Flat is interpreted by server as boolean True.
 
+\par Keys
 Standard keys understood by server are predefined in pad.codes.CODES dict.
-Some of those keys are required to be present in message so that it won't be
-rejected in further processing.
+It's recommended for key part to be a single code in char representation
+(key's code encoded by chr(key_int) and decoded by ord(key_char)). String
+codes may also proceeded, though.
+
+There are some keys required to be present in message so that it won't be
+rejected in further processing. Those are indicated in pad.codes.CODES docs.
+
+Key ought to be one of keys listed in pad.codes.CODES. Custom keys are also
+proceeded, but may not be understood and handled by server.
 """
 from pad import codes
 
 UNIT_DELIMTR = 2 * u"\u001F"
 INNER_DELIMTR = u"\u001F"
+
+## The only (unstable) version supported for the moment of writing
+VERSION = 1
 
 
 def decode(record):
@@ -42,22 +56,29 @@ def decode(record):
     @returns{dict(string,string|data|boolean)} key-value pairs of received data;
     - for <b>key-value</b> pair value is string,
     - for <b>flag key</b> value is boolean True.
+    @return{None} if decode fails (eg. unsupported version provided)
     """
-    data = {}
-    units = {unit for unit in record.split(UNIT_DELIMTR)}
-    for unit in units:
-        pair = unit.split(INNER_DELIMTR)
-        key = None
-        try:
-            key = int(pair[0])
-            key = codes.label(key)
-        except Exception:  # handeled in the following if
-            pass
-        if not key:
-            key = str(pair())
-
-        data[key] = len(pair) == 1 or pair[1].decode('base64')
-    return data
+    try:
+        version = ord(record[0])
+    except Exception:
+        return None
+    if version == VERSION:
+        data = {}
+        units = {unit for unit in record[1:-1].split(UNIT_DELIMTR)}
+        for unit in units:
+            pair = unit.split(INNER_DELIMTR)
+            key = None
+            try:
+                key = ord(pair[0])
+                key = codes.label(key)
+            except Exception:  # handeled in the following if
+                pass
+            if not key:
+                key = str(pair())
+            data[key] = len(pair) == 1 or pair[1].decode('base64')
+        return data
+    else:
+        return None
 
 
 def encode(data):
@@ -67,4 +88,4 @@ def encode(data):
     - for <b>key-value pair</b> value is string or data
     - for <b>flag key</b> value is boolean True
     """
-    return (UNIT_DELIMTR).join("%s%s%s" % (str(codes.code(key)), "" if data[key] == True else INNER_DELIMTR, data[key].encode('base64') if data[key] != True  else "") for key in data)
+    return chr(VERSION) + (UNIT_DELIMTR).join("%s%s%s" % (chr(codes.code(key)), "" if data[key] == True else INNER_DELIMTR, data[key].encode('base64') if data[key] != True  else "") for key in data)
