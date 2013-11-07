f99cfa02cb819a284dc73bb3999c86ad7e0c2947
Zosia <sobocinska.zosia@gmail.com>
2013-11-07 02:51:28 +0100

    Zaślepka favicony

--- server/academia/urls.py
+++ server/academia/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+from academia import settings
 admin.autodiscover()
 
 urlpatterns = patterns('')
@@ -10,6 +12,7 @@ urlpatterns += patterns('',
     url(r'^$', "channels", name="channels"),
     url(r'^channels/', include('channels.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^favicon\.ico$', RedirectView.as_view(url=settings.MEDIA_URL + 'static/images/favicon.ico')),
 
     # \url(r'^$', 'academia.views.home', name='home'),
     # url(r'^academia/', include('academia.foo.urls')),
