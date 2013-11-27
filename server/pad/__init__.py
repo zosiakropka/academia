f8abf2237ac169983acb81bca9525c857e84ad45
Zosia <sobocinska.zosia@gmail.com>
2013-11-27 18:10:32 +0100

    Remove useless imports

--- server/pad/__init__.py
+++ server/pad/__init__.py
@@ -4,8 +4,6 @@
 """
 
 from pad.server.padserver import PadWSServer, PadTCPServer
-from pad.server.padthread import PadServerThread
-from threading import Thread
 
 tcpserver = PadTCPServer("0.0.0.0", 5001)  # @todo: shouldn't be hardcoded
 tcpserver.start_server()
