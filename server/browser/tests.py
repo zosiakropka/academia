fcc1dfaaacee1ee5eba0417a5ad2d906051fcf3d
Zosia <sobocinska.zosia@gmail.com>
2013-12-07 04:19:41 +0100

    Tests stub replaced with @todo

--- server/browser/tests.py
+++ server/browser/tests.py
@@ -2,18 +2,5 @@
 @author: Zosia Sobocinska
 @date Nov 26, 2013
 
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+@todo: tests
 """
-
-from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
