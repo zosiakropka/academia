5d9295b3a9ae0a8b37a8e02f94cb765b68ecf85d
Zosia <sobocinska.zosia@gmail.com>
2013-12-07 04:23:48 +0100

    Tests stub replaced with @todo

--- server/browser/urls.py
+++ server/browser/urls.py
@@ -6,8 +6,6 @@
 from django.conf.urls import patterns, url
 
 from django.contrib import admin
-from django.views.generic.list import ListView
-from browser.models import Subject
 admin.autodiscover()
 
 urlpatterns = patterns('')
