c75907b9eae010d046ee0d03d21f19c12d9b2814
Zosia <sobocinska.zosia@gmail.com>
2013-12-09 17:53:56 +0100

    academia.urls order fix

--- server/academia/urls.py
+++ server/academia/urls.py
@@ -16,12 +16,14 @@ urlpatterns = patterns('')
 urlpatterns += patterns('',
 
     (r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico')),
-    url(r'^', include('browser.urls')),
+
     url(r'^account/', include('account.urls')),
-    url(r'^pad/', include('pad.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^', include('browser.urls')),
+    url(r'^pad/', include('pad.urls')),
 )
+
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += staticfiles_urlpatterns()
