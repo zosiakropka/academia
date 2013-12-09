2520525f27cbf1d0f070fc18d19df2c5ef9224d1
Zosia <sobocinska.zosia@gmail.com>
2013-12-09 17:52:41 +0100

    Static files settings fixed

--- server/academia/urls.py
+++ server/academia/urls.py
@@ -15,10 +15,14 @@ admin.autodiscover()
 urlpatterns = patterns('')
 urlpatterns += patterns('',
 
+    (r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico')),
     url(r'^', include('browser.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^pad/', include('pad.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    (r'^favicon\.ico$', RedirectView.as_view(url=settings.MEDIA_URL + 'static/images/favicon.ico')),
 )
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+
