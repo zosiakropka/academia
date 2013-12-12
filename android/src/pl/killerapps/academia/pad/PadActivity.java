6005924fd7491eb6bd8ca92c5e32f04af6068251
Zosia <sobocinska.zosia@gmail.com>
2013-12-12 08:45:32 +0100

    Just test whether TCP connection works bi-direct

--- android/src/pl/killerapps/academia/pad/PadActivity.java
+++ android/src/pl/killerapps/academia/pad/PadActivity.java
@@ -1,13 +1,12 @@
 package pl.killerapps.academia.pad;
 
+import java.io.IOException;
+
 import pl.killerapps.academia.ConnectActivity;
 import pl.killerapps.academia.R;
-import pl.killerapps.academia.R.layout;
-import pl.killerapps.academia.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
 
 public class PadActivity extends Activity {
@@ -18,17 +17,26 @@ public class PadActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pad);
-		
+			
 		Bundle extras = getIntent().getExtras();
 		String ip = extras.getString("ip");
 		int appPort = extras.getInt("port");
+
 		int socketPort = 5001; // TODO: should be obtained via application
-		
+
 		client = new PadClient(ip, socketPort) {
 			
 			@Override
 			public void onMessage(PadMessage message) {
-				Log.i("PadSocket", message.get("purpose"));
+				try {
+					message.set_string("purpose", "test");
+					message.set_string("message", "got it");
+					send(message);
+				} catch (UnsupportedOperationException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				
 			}
 			@Override
@@ -37,7 +45,6 @@ public class PadActivity extends Activity {
 			}
 		};
 		client.start();
-		
 	}
 
 	@Override
