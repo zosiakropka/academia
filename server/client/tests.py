0d3c5d5ae968eedd844641bb80880f1b13b58ba7
Zosia <sobocinska.zosia@gmail.com>
2013-12-13 06:47:27 +0100

    [Server] Tiny fixes

--- server/client/tests.py
+++ server/client/tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-"""@package backbone.tests
+"""@package client.tests
 @author: Zosia Sobocinska
 @date Nov 26, 2013
 
--- server/utils/decorators.py
+++ server/utils/decorators.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 """@package utils.decorators
 @author Zosia Sobocinska
 @date Dec 11, 2013
