46d414537f81a10cfe50be0e1d79db3ae91f7160
Zosia <sobocinska.zosia@gmail.com>
2013-12-15 18:01:26 +0100

    [server] notes api

--- server/api/views/note.py
+++ server/api/views/note.py
@@ -3,23 +3,64 @@
 @date Dec 14, 2013
 """
 from utils.decorators import authenticate, api
-from backbone.models import Subject
-from utils.serializer import query_to_dict, JsonEncoder
-import json
+from backbone.models import Subject, Activity, Note
+from utils.serializer import query_to_list, JsonEncoder, model_to_dict
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 
 @authenticate(user=True, admin=True)
 @api
-def note_create(user):
-    pass
+def note_create(user, note_access, subject_name=None, subject_abbr=None, activity_type=None):
+    activity = get_object_or_404(Activity, subject_name=None, subject_abbr=None, activity_type=None)
+    note = Note(access=note_access, activity=activity, owner=user)
+    note.save()
+    note = model_to_dict(note)
+    return JsonEncoder().encode(note)
 
 
 @authenticate(user=True, admin=True)
 @api
-def note_list(user, subject_abbr=None, activity_type=None):
+def note_list(user, subject_name=None, subject_abbr=None, activity_type=None):
+    """
+    @todo Rewrite it entirely so that select starts from notes
+    """
 
     subjects = Subject.objects.all()
-    subjects = query_to_dict(subjects, exclude_relations={'activities': {'notes': {'content': None}}})
+    if subject_name:
+        subject_name = subject_name.pop()
+        subjects = subjects.filter(name=subject_name)
+    if subject_abbr:
+        subject_abbr = subject_abbr.pop()
+        subjects = subjects.filter(abbr=subject_abbr)
+
+    if subjects.count() == 1 and activity_type:
+        activity_type = activity_type.pop()
+        subject = subjects.get()
+        activity = subject.activities.get(type=activity_type)
+        notes = activity.get_accessible_notes(user=user)
+        notes = query_to_list(notes, exclude_relations={'content': None})
+        activity = model_to_dict(activity, exclude_relations={'notes': None})
+        activity['notes'] = notes
+        subject = model_to_dict(subject, exclude_relations={'activities': None})
+        subject['activities'] = [activity]
+        subjects = [subject]
+    else:
+        subjects_list = []
+        for subject in subjects:
+            activities = subject.activities.all()
+            activities_list = []
+            for activity in activities:
+                notes = activity.get_accessible_notes(user=user)
+                notes = query_to_list(notes, exclude_relations={'content': None})
+                activity = model_to_dict(activity, exclude_relations={'notes': None})
+                activity['notes'] = notes
+                activities_list.append(activity)
+            subject = model_to_dict(subject, exclude_relations={'activities': None})
+            subject['activities'] = activities_list
+            subjects_list.append(subject)
+        subjects = subjects_list
+
     return JsonEncoder().encode(subjects)
 
 
