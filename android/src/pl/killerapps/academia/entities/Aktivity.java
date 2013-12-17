a57eded4e1a5b7198c52ba7de8a47e90b1af1925
Zosia <sobocinska.zosia@gmail.com>
2013-12-17 17:03:12 +0100

    [client] Activity entity

---
+++ android/src/pl/killerapps/academia/entities/Aktivity.java
@@ -0,0 +1,35 @@
+package pl.killerapps.academia.entities;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * "k" in Aktivity comes into play to distinguish between Android Activity and
+ * Activity entity in Academia notes hierarchy.
+ *
+ * @author zosia
+ *
+ */
+public class Aktivity {
+
+    String supervisor;
+    String type;
+    List<Note> notes;
+
+    public static final String LECTURE = "lect";
+    public static final String LABORATORIUM = "lab";
+    public static final String PROJECT = "proj";
+    public static final String CONVERSATORY = "conv";
+    public static final String FOREIGN_LANGUAGE = "lang";
+    public static final String EXERCISE = "exer";
+    public static final HashMap<String, String> TYPES = new HashMap<String, String>();
+
+    {
+        TYPES.put(LECTURE, "Lecture");
+        TYPES.put(LABORATORIUM, "Laboratorium");
+        TYPES.put(PROJECT, "Project");
+        TYPES.put(CONVERSATORY, "Conversatory");
+        TYPES.put(FOREIGN_LANGUAGE, "Foreign language");
+        TYPES.put(EXERCISE, "Exercise");
+    }
+}
