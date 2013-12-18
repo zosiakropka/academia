39ea8fdd37db0db36f52f502d29c51aa67c00abf
Zosia <sobocinska.zosia@gmail.com>
2013-12-18 13:55:27 +0100

    [client] api SubjectList (doesn't work since not authenticated)

---
+++ android/src/pl/killerapps/academia/api/command/subject/SubjectList.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.killerapps.academia.api.command.subject;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import org.json.JSONArray;
+import pl.killerapps.academia.api.command.ApiCommand;
+import pl.killerapps.academia.entities.Subject;
+
+/**
+ *
+ * @author zosia
+ */
+public abstract class SubjectList extends ApiCommand<List<Subject>> {
+
+    public SubjectList(String base_url) throws URISyntaxException {
+        super(base_url, "/subject/list/");
+    }
+
+    @Override
+    protected List<Subject> process_json(JSONArray json) {
+
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
