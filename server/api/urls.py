09fcb4a53575e42eec5b0e34cba3476c31651d1d
Zosia <sobocinska.zosia@gmail.com>
2013-12-18 13:45:35 +0100

    [server] urls for subject* and activity* api commands

--- server/api/urls.py
+++ server/api/urls.py
@@ -15,4 +15,10 @@ urlpatterns += patterns('api.views',
     url(r'^note/list/$', 'note_list'),
     url(r'^note/get/$', 'note_get'),
     url(r'^note/create/$', 'note_create'),
+
+    url(r'^subject/list/$', 'subject_list'),
+    url(r'^subject/get/$', 'subject_list'),
+    url(r'^subject/list/$', 'subject_list'),
+
+    url(r'^activity/list/$', 'activity_list'),
 )
