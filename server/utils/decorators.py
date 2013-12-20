44304f329ca302381b73eb2450df01b24c64af11
Zosia <sobocinska.zosia@gmail.com>
2013-12-20 07:25:34 +0100

    [server] api_auth decorator

--- server/utils/decorators.py
+++ server/utils/decorators.py
@@ -8,11 +8,13 @@ from django.shortcuts import render
 from academia.settings import LOGIN_URL
 from functools import wraps
 from django.http.response import HttpResponse, HttpResponseRedirect
-from django.utils import simplejson
 from django.core.exceptions import PermissionDenied
 
 
 def authenticate(user=False, admin=False):
+    """
+    @todo move this to client utils
+    """
     def _decorator(fun):
         allowed = {'user': user, 'admin': admin}
 
@@ -31,6 +33,9 @@ def authenticate(user=False, admin=False):
 
 
 def abstractor(fun):
+    """
+    @todo move this to client utils
+    """
     @wraps(fun)
     def _wrapper(user, request, **kwargs):
         parameters = request.POST if request.method == 'POST' else request.GET
@@ -39,19 +44,40 @@ def abstractor(fun):
         result = fun(user, **kwargs)
         try:
             template, data = result
+            return render(request, template, data)
         except:
             return result
-        return render(request, template, data)
     return _wrapper
 
 
 def api(fun):
+    """
+    @todo move this to api utils
+    """
     @wraps(fun)
     def _wrapper(user, request, **kwargs):
         parameters = request.POST if request.method == 'POST' else request.GET
         data = fun(user, **dict(parameters))
         return HttpResponse(data, mimetype='application/json')
     return _wrapper
+
+
+def api_auth(user=False, admin=False):
+    """
+    @todo move this to api utils
+    """
+    def _decorator(fun):
+        allowed = {'user': user, 'admin': admin}
+
+        @wraps(fun)
+        def _wrapper(request, *args, **kwargs):
+            user = request.user
+            if user and user.is_authenticated():
+                if (allowed['user']) or (allowed['admin'] and user.is_superuser):
+                    return fun(user, request, *args, **kwargs)
+            raise PermissionDenied()
+        return _wrapper
+    return _decorator
 
 
 def not_implemented(fun):
