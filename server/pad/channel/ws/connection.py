e392aba69d186a6dcdd04e82167a18a28f7dbf06
Zosia <sobocinska.zosia@gmail.com>
2014-02-03 06:08:19 +0100

    [server] PadWSConnection connects to PadTCPServer via PadTCPProxy

--- server/pad/channel/ws/connection.py
+++ server/pad/channel/ws/connection.py
@@ -6,14 +6,17 @@
 """
 from ws4py.websocket import WebSocket
 from pad.channel.base.connection import PadBaseConnection
-
+from pad.channel.tcp.proxy import PadTCPProxy
 
 class PadWSConnection(WebSocket, PadBaseConnection):
 
     def __init__(self, sock, protocols=None, extensions=None, environ=None, heartbeat_freq=None):
+
         WebSocket.__init__(self, sock, protocols=protocols, extensions=extensions, environ=environ,
                            heartbeat_freq=heartbeat_freq)
         PadBaseConnection.__init__(self)
+
+        self.tcp_client = PadTCPProxy(self)
 
     def received_message(self, message):
         self.handle_message(str(message))
