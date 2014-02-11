e56803fa5bdcf28c219e7d77fd5fe5e16f9d64b9
Zosia <sobocinska.zosia@gmail.com>
2014-02-11 18:01:11 +0100

    note get command

--- android/src/pl/killerapps/academia/api/command/note/NoteGet.java
+++ android/src/pl/killerapps/academia/api/command/note/NoteGet.java
@@ -3,6 +3,10 @@ package pl.killerapps.academia.api.command.note;
 import java.net.URISyntaxException;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
 
 import pl.killerapps.academia.api.command.ApiCommandAsync;
 import pl.killerapps.academia.entities.Note;
@@ -15,8 +19,22 @@ public abstract class NoteGet extends ApiCommandAsync<Note> {
   }
 
   @Override
-  protected Note process_json(JSONArray json) {
-    throw new UnsupportedOperationException("Not supported yet.");
+  protected Note process_json(JSONArray notes_json) {
+	  Note note = new Note();
+	  if (notes_json.length() == 1) {
+		  try {
+			JSONObject note_json = notes_json.getJSONObject(0);
+			note.access = note_json.getString("access");
+			note.id = note_json.getInt("pk");
+			note.aktivity_id = note_json.getInt("activity");
+			note.title = note_json.getString("title");
+			note.content = note_json.getString("content");
+			// @todo fill missing parts
+		} catch (JSONException e) {
+	        Log.e("academia_api", "Can't parse", e);
+		}
+	  }
+	  return note;
   }
 
 }
