c3471d4a3e80eaeaac6275d19a398b9491b684d3
Zosia <sobocinska.zosia@gmail.com>
2014-02-22 19:27:50 +0100

    [server] better api.auth

--- server/api/views/auth.py
+++ server/api/views/auth.py
@@ -2,9 +2,6 @@
 @author Zosia Sobocinska
 @date Dec 20, 2013
 """
-from django.core.context_processors import csrf
-from django.shortcuts import render_to_response
-import logging
 from django.http.response import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.core.exceptions import PermissionDenied
@@ -14,11 +11,7 @@ def auth_csrf(request):
     """
     @todo find out why this requires template and I just can't send 201
     """
-    c = {}
-    ctkn = csrf(request)
-    logging.info(unicode(ctkn['csrf_token']))
-    c.update(csrf(request))
-    return render_to_response("api/csrftoken.html", c)
+    return HttpResponse(status=201)
 
 
 def signin(request):
@@ -39,4 +32,4 @@ def signin(request):
     if failure:
         raise PermissionDenied()
     else:
-        return HttpResponse(201)
+        return HttpResponse(status=201)
