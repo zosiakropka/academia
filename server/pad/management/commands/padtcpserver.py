12bd25dee183736b196bbdb8c6fd1559e6fbe550
Zosia <sobocinska.zosia@gmail.com>
2014-05-14 09:59:26 +0200

    Now it works

--- server/pad/management/commands/padtcpserver.py
+++ server/pad/management/commands/padtcpserver.py
@@ -12,9 +12,13 @@ class Command(DaemonCommand):
     args = ''
     help = 'Runs TCP server to enable real time collaboration via TCP sockets'
 
-    def handle(self, *args, **options):
-        from pad.channel.tcp.server import PadTCPServer
+    PID_FILE = "academia_padtcpserver.pid"
+    LOG_FILE = "academia_padtcpserver.log"
+
+    def loop_callback(self, *args, **options):
         from academia.settings import PADSERVERS
+
+        from pad.channel.tcp.server import PadTCPServer
         server = PADSERVERS['tcpserver']
 
         tcpserver = PadTCPServer(server['host'], server['port'])
--- server/pad/management/commands/padwsproxy.py
+++ server/pad/management/commands/padwsproxy.py
@@ -12,9 +12,13 @@ class Command(DaemonCommand):
     args = ''
     help = 'Runs WS server to enable real time collaboration via WS sockets'
 
-    def handle(self, *args, **options):
-        from pad.channel.ws.proxy import PadWSProxy
+    PID_FILE = "academia_padwsproxy.pid"
+    LOG_FILE = "academia_padwsproxy.log"
+
+    def loop_callback(self):
         from academia.settings import PADSERVERS
+
+        from pad.channel.ws.proxy import PadWSProxy
         server = PADSERVERS['wsproxy']
 
         wsserver = PadWSProxy(server['host'], server['port'])
