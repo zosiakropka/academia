8ac378f65b1abc3e210a2d46cf82951b704e51ab
Zosia <sobocinska.zosia@gmail.com>
2014-05-14 10:34:01 +0200

    More Googlish stuff commented

--- server/backbone/admin.py
+++ server/backbone/admin.py
@@ -7,12 +7,12 @@
 
 from django.contrib import admin
 from backbone.models import Subject, Supervisor, Activity, Note, NoteModification, Schedule
-from backbone.models.google_calendar import GoogleCalendar
+# from backbone.models.google_calendar import GoogleCalendar
 
 admin.site.register(Activity)
 admin.site.register(Supervisor)
 admin.site.register(Subject)
 admin.site.register(Note)
 admin.site.register(NoteModification)
-admin.site.register(GoogleCalendar)
+# admin.site.register(GoogleCalendar)
 admin.site.register(Schedule)
